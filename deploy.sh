e57f398f67cbce9a31b967ed0b627ee9b3018d9d
peizijune <1214924488@qq.com>
2021-09-27 18:28:04 +0800

    Update deploy.sh
    fix typo

--- deploy.sh
+++ deploy.sh
@@ -1,45 +1,41 @@
 #!/usr/bin/env bash
-#编译+部署order站点
+# 编译+部署order站点
 
-#需要配置如下参数
+# 需要配置如下参数
 # 项目路径, 在Execute Shell中配置项目路径, pwd 就可以获得该项目路径
 # export PROJ_PATH=这个jenkins任务在部署机器上的路径
 
 # 输入你的环境上tomcat的全路径
 # export TOMCAT_APP_PATH=tomcat在部署机器上的路径
 
-### base 函数
+# 自定义的Shell函数
 killTomcat()
 {
-    pid=`ps -ef|grep tomcat|grep java|awk '{print $2}'`
-    echo "tomcat Id list :$pid"
+    pid =`ps -ef|grep tomcat|grep java|awk '{print $2}'`
+    echo "tomcat PID list: $pid"
     if [ "$pid" = "" ]
     then
-      echo "no tomcat pid alive"
+      echo "no tomcat PID alive"
     else
       kill -9 $pid
     fi
 }
-cd $PROJ_PATH/order
-mvn clean install
 
-# 停tomcat
+# 进入项目目录
+cd $PROJ_PATH/order
+# maven打包到本地仓库
+mvn clean install
+# 停止Tomcat
 killTomcat
 
-# 删除原有工程
+# 部署应用程序：删除原有项目，复制新的项目并改名为ROOT.war
 rm -rf $TOMCAT_APP_PATH/webapps/ROOT
 rm -f $TOMCAT_APP_PATH/webapps/ROOT.war
 rm -f $TOMCAT_APP_PATH/webapps/order.war
-
-# 复制新的工程
 cp $PROJ_PATH/order/target/order.war $TOMCAT_APP_PATH/webapps/
-
 cd $TOMCAT_APP_PATH/webapps/
 mv order.war ROOT.war
 
 # 启动Tomcat
 cd $TOMCAT_APP_PATH/
 sh bin/startup.sh
-
-
-
